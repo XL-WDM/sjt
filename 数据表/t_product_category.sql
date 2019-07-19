44e18f25db19b9821126a20e6e09e6e2559772e4
XL-WDM <15073820717@163.com>
2019-07-19 23:42:15 +0800

    编码

--- 数据表/t_product_category.sql
+++ 数据表/t_product_category.sql
@@ -22,8 +22,8 @@ create index t_product_category_t_product_category_id_fk
 	on t_product_category (pid)
 ;
 
-INSERT INTO stj.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (1, '山田产品', 1, 1, null, null, null, null, '2019-07-19 10:57:11', '2019-07-19 10:57:11');
-INSERT INTO stj.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (2, '手作农产品', 2, 1, null, null, null, 1, '2019-07-19 10:57:11', '2019-07-19 10:57:11');
-INSERT INTO stj.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (3, '随饮小憩系列', 3, 1, null, 'http://localhost/images/banner5.png', 'https://www.baidu.com/', 2, '2019-07-11 15:27:59', '2019-07-11 15:27:59');
-INSERT INTO stj.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (4, '健康蔬果膏系列', 3, 1, null, 'http://localhost/images/banner5.png', 'https://www.baidu.com/', 2, '2019-07-11 15:27:59', '2019-07-11 15:27:59');
-INSERT INTO stj.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (5, '养生果膏系列', 3, 1, null, 'http://localhost/images/banner5.png', 'https://www.baidu.com/', 2, '2019-07-11 17:23:41', '2019-07-11 17:23:41');
+INSERT INTO sjt.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (1, '山田产品', 1, 1, null, null, null, null, '2019-07-19 10:57:11', '2019-07-19 10:57:11');
+INSERT INTO sjt.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (2, '手作农产品', 2, 1, null, null, null, 1, '2019-07-19 10:57:11', '2019-07-19 10:57:11');
+INSERT INTO sjt.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (3, '随饮小憩系列', 3, 1, null, 'http://localhost/images/banner5.png', 'https://www.baidu.com/', 2, '2019-07-11 15:27:59', '2019-07-11 15:27:59');
+INSERT INTO sjt.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (4, '健康蔬果膏系列', 3, 1, null, 'http://localhost/images/banner5.png', 'https://www.baidu.com/', 2, '2019-07-11 15:27:59', '2019-07-11 15:27:59');
+INSERT INTO sjt.t_product_category (id, category_name, category_level, status, icon, img_url, url, pid, create_date, update_date) VALUES (5, '养生果膏系列', 3, 1, null, 'http://localhost/images/banner5.png', 'https://www.baidu.com/', 2, '2019-07-11 17:23:41', '2019-07-11 17:23:41');
